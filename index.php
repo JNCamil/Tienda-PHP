9721a8cad9a6abfc2e5150577a9aff2fe7c8bb49
JNCamil <juancamvh@hotmail.com>
2024-04-03 13:13:09 +0200

    css barra lateral

--- index.php
+++ index.php
@@ -51,17 +51,20 @@
 
             <aside id="lateral">
                 <div id="login" class="block_aside">
+                    <h3>Entrar a la web</h3>
                     <form action="#" method="post">
                         <label for="email">Email</label>
-                        <input type="email" name="email">
+                        <input type="email" name="email" />
                         <label for="password">Contraseña</label>
                         <input type="password" name="password">
                         <input type="submit" value="Enviar">
 
                     </form>
-                    <a href="#">Mis pedidos</a>
-                    <a href="#">Gestionar pedidos</a>
-                    <a href="#">Gestionar categorías</a>
+                    <ul>
+                        <li><a href="#">Mis pedidos</a></li>
+                        <li><a href="#">Gestionar pedidos</a></li>
+                        <li><a href="#">Gestionar categorías</a></li>
+                    </ul>
 
 
                 </div>
